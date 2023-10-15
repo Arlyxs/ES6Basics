15d0d40f7fecb664aec81977d1a1bea8ed777d06
Arlyxs <arlyxs@gmail.comgit.config --global user.email>
2023-10-15 05:52:34 -0400

    endo f lesson arrays with arrow functions

--- es6.js
+++ es6.js
@@ -92,3 +92,29 @@ const activeJobs = jobs.filter(function(job) {return job.isActive;});
 const activeJobs1 = jobs.filter(job => job.isActive);
 console.log(activeJobs);
 console.log(activeJobs1);
+
+//set timeout executes the referenced function with a delay in miliseconds.  1000ms = 1 second
+//by defining self as this in the talk function it binds the this keyword to the talk function as opposed to the global window.
+
+const person6 = {
+  talk() {
+    let self = this;
+    setTimeout(() => {
+        console.log('this', this);
+    }, 1000);
+ }
+};
+    
+person6.talk();
+
+/* Arrays Map - used often in React/Map method of array is used to render a list of items from the array  */
+
+/* old function expression
+const items = colors.map(color => '<li>' + color + '<li>');es6 uses temperate literals to get rid of the concatinations by defining a template for output
+const items = colors.map(color => '<li' + color + '<li>'); 
+${} is an argument placeholder.  what is placed in the curly braces willbe rendered dynamically at runtime*/
+const colors = ['red', 'green', 'blue' ];
+const items = colors.map(color => `<li>${color}</li>`);
+console.log(items);
+
+

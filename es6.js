09fc851e366258100adec7b5df3482067f1cd00b
Arlyxs <arlyxs@gmail.com>
2023-10-15 20:24:55 -0400

    end of inheritance lesson

--- es6.js
+++ es6.js
@@ -186,3 +186,30 @@ console.log(persons.name);
 console.log(persons.walk);
 console.log(person8.name);
 
+//INHERITANCE in classes
+class Person1 {
+  constructor(name) {
+      this.name = name;
+  }
+  
+  walk() {
+    console.log("walk");
+  }
+}
+
+/* The new Teacher class inherits the methods defined in the Person1 class using the extend keyword.  If a constructor is used in Teacher class then need to use the "super" keyword to activate the parameter from the parent class. */
+class Teacher extends Person1 {
+  constructor(name, degree) {
+      super(name);
+      this.degree = degree;
+  }
+  
+  teach() {
+      console.log('teach');
+  }
+}
+//pass arguments to teacher, "teacher." has all parameters of Person and Teacher classes
+const teacher = new Teacher('Lyxi', 'AAS');
+
+
+
--- tester.js
+++ tester.js
@@ -1,17 +1,5 @@
-//Classes, constructor
-const person8 = {
-  name: "Clair",
-  walk() {
-    console.log("walk");
-  },
-};
-
-/* when a method is duplicated runs the risk of causing problems in all statements that use the method eg - if we wish to create another person object */
-
-/* a blueprint to create object of the same type is done using classes.  
-classes use post call naming convention (every word starts with caps eg class = CoolPerson.
-use a constructor to create the objects in the class (with parameters) */
-class Person {
+//INHERITANCE in classes
+class Person1 {
     constructor(name) {
         this.name = name;
     }
@@ -21,8 +9,17 @@ class Person {
     }
 }
 
-//when creating an object from a class need to use the new operator.
-const person = new Person('Clair2');
-console.log(person.name);
-console.log(person.walk);
-console.log(person8.name);
+/* The new Teacher class inherits the methods defined in the Person1 class using the extend keyword.  If a constructor is used in Teacher class then need to use the "super" keyword to activate the parameter from the parent class. */
+class Teacher extends Person1 {
+    constructor(name, degree) {
+        super(name);
+        this.degree = degree;
+    }
+    
+    teach() {
+        console.log('teach');
+    }
+}
+//pass arguments to teacher, "teacher." has all parameters of Person and Teacher classes
+const teacher = new Teacher('Lyxi', 'AAS');
+
